7d648b4cc35f14013ed17f2f0f29913cba3baa69
goosetea04 <gusti.fatu@gmail.com>
2024-02-13 14:47:42 +0700

    fixed one issue fto maintain code quality in the codebase

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -1,8 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
+import org.springframework.beans.factory.annotation.Autowired; //code quality fix: removed unused imports
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
